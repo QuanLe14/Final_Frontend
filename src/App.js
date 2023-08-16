b7ef27f676a61b6fb117a956d41fb20e109aadc5
Le <lem19@student.douglascollege.ca>
2023-08-16 13:41:40 -0700

    front commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import BookList from './components/FrontPage_BookList';
+import CreateBook from './components/CreateBook';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/create-book" element={<CreateBook />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
